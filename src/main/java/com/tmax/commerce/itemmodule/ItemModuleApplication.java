b49be96cbfe68c43c315280919de1d6c3fdf0a84
hoyong1ee <hoyong_lee2@tmax.co.kr>
2023-08-18 15:16:06 +0900

    project initialization

---
+++ src/main/java/com/tmax/commerce/itemmodule/ItemModuleApplication.java
@@ -0,0 +1,13 @@
+package com.tmax.commerce.itemmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemModuleApplication.class, args);
+	}
+
+}
