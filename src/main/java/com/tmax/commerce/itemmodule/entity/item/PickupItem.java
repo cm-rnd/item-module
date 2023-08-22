b8900f5b467441d8cfdfe2535498a2ad0bc64a21
gyeongtae <37093814+Bombada@users.noreply.github.com>
2023-08-22 16:39:52 +0900

    feat: subtype Item

---
+++ src/main/java/com/tmax/commerce/itemmodule/entity/item/PickupItem.java
@@ -0,0 +1,21 @@
+package com.tmax.commerce.itemmodule.entity.item;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.File;
+import java.util.List;
+
+@Getter
+@Entity
+@DiscriminatorValue("PICKUP")
+@NoArgsConstructor
+public class PickupItem extends Item {
+    @Builder
+    public PickupItem(int price, boolean brandNew, boolean recommended, ItemStatus itemStatus, String name, String description, List<File> productImages, Item.SuperStoreStatus connectedSuperStore, int sequence) {
+        super(price, brandNew, recommended, itemStatus, name, description, productImages, connectedSuperStore, sequence, OrderType.PICKUP);
+    }
+}
