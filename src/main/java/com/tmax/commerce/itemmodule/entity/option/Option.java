91c67594a3c621e55e564e6444ca83b3f848f988
ramboramboram <boram_kim3@tmax.co.kr>
2023-08-22 18:30:03 +0900

    [refactor] itemCode, Optiontype 필드 추가 및 데이터타입 변경

--- src/main/java/com/tmax/commerce/itemmodule/entity/option/Option.java
+++ src/main/java/com/tmax/commerce/itemmodule/entity/option/Option.java
@@ -1,6 +1,7 @@
 package com.tmax.commerce.itemmodule.entity.option;
 
-import com.tmax.commerce.itemmodule.entity.base.BaseEntity;
+import com.tmax.commerce.itemmodule.entity.base.DateTimeEntity;
+import com.tmax.commerce.itemmodule.entity.item.ItemStatus;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -10,17 +11,19 @@ import java.util.UUID;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Entity(name = "`option`")
-public class Option extends PersistableDateTimeEntity {
+@Entity(name = "option")
+public class Option extends DateTimeEntity {
     @Id
     @Column(columnDefinition = "BINARY(16)")
     private UUID id;
 
-    private Long originId;
+    private String itemCode; // nullable가능함
+
+    private OptionType type;// TODO: Bang 추후 테스트 코드 작성
 
     private String name;
 
-    private Long price;
+    private int price;
 
     private int sequence;
 
@@ -28,7 +31,7 @@ public class Option extends PersistableDateTimeEntity {
     @JoinColumn(name = "option_group_id")
     private OptionGroup optionGroup;
 
-    private Long versionNumber;
+    private Long version;
 
     @Enumerated(EnumType.STRING)
     private ItemStatus itemStatus;
