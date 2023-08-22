b04d8fbc5a3aa2305f84a707afec6495c9ffb97f
ramboramboram <boram_kim3@tmax.co.kr>
2023-08-22 18:31:08 +0900

    [etc] 디렉토리 변경

--- src/main/java/com/tmax/commerce/itemmodule/entity/item/subtyped/OnsiteItem.java
+++ src/main/java/com/tmax/commerce/itemmodule/entity/item/subtyped/OnsiteItem.java
@@ -1,5 +1,8 @@
-package com.tmax.commerce.itemmodule.entity.item;
+package com.tmax.commerce.itemmodule.entity.item.subtyped;
 
+import com.tmax.commerce.itemmodule.entity.item.Item;
+import com.tmax.commerce.itemmodule.entity.item.ItemStatus;
+import com.tmax.commerce.itemmodule.entity.item.OrderType;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Builder;
