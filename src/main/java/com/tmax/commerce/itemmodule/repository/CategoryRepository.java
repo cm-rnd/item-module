326df52e7211a69e82102a038833010c8067b983
jaehoon-prod <jaehoon_choi3@tmax.co.kr>
2023-08-23 15:56:00 +0900

    chore : category 레포 변경

--- src/main/java/com/tmax/commerce/itemmodule/repository/CategoryRepository.java
+++ src/main/java/com/tmax/commerce/itemmodule/repository/CategoryRepository.java
@@ -2,7 +2,14 @@ package com.tmax.commerce.itemmodule.repository;
 
 import com.tmax.commerce.itemmodule.entity.category.Category;
 
-import java.util.UUID;
+import java.util.List;
 
-public interface CategoryRepository extends ExtendedRepository<Category, UUID> {
+public interface CategoryRepository extends ExtendedRepository<Category, Long>, CategoryRepositoryCustom {
+
+    List<Category> findByParentCategoryIsNull();
+    List<Category> findByName(String name);
+
+    Boolean existsByName(String name);
+
+
 }
