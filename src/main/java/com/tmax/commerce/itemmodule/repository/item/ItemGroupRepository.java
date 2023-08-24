598128434d11562a0539a9dc8ee34b6aa05cba6d
ramboramboram <boram_kim3@tmax.co.kr>
2023-08-24 16:11:04 +0900

    [feat] itemGroupRepositry 생성

---
+++ src/main/java/com/tmax/commerce/itemmodule/repository/item/ItemGroupRepository.java
@@ -0,0 +1,7 @@
+package com.tmax.commerce.itemmodule.repository.item;
+
+import com.tmax.commerce.itemmodule.entity.item.ItemGroup;
+import com.tmax.commerce.itemmodule.repository.ExtendedRepository;
+
+public interface ItemGroupRepository extends ExtendedRepository<ItemGroup, Long> {
+}
