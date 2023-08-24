5ec5feb567937e80fe9f6d124291e6e6881ecd02
ramboramboram <boram_kim3@tmax.co.kr>
2023-08-24 16:07:23 +0900

    [feat] 필요 메서드 추가

--- src/main/java/com/tmax/commerce/itemmodule/entity/option/Option.java
+++ src/main/java/com/tmax/commerce/itemmodule/entity/option/Option.java
@@ -3,10 +3,7 @@ package com.tmax.commerce.itemmodule.entity.option;
 import com.tmax.commerce.itemmodule.entity.base.DateTimeEntity;
 import com.tmax.commerce.itemmodule.entity.item.ItemStatus;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.UUID;
 
@@ -37,8 +34,46 @@ public class Option extends DateTimeEntity {
     @JoinColumn(name = "option_group_id")
     private OptionGroup optionGroup;
 
-    private Long version;
+    private long version = 0;
 
     @Enumerated(EnumType.STRING)
     private ItemStatus itemStatus;
+
+    private void updateVersion() {
+        version++;
+    }
+
+    @Builder
+    public Option(OptionGroup optionGroup, String name, int price, ItemStatus itemStatus, int sequence) {
+        this.optionGroup = optionGroup;
+        this.name = name;
+        this.price = price;
+        this.itemStatus = itemStatus;
+        this.sequence = sequence;
+    }
+
+    public void changeName(String name) {
+        if (!this.name.equals(name)) {
+            this.name = name;
+            updateVersion();
+        }
+    }
+
+    public void changePrice(int price) {
+        if (this.price != price) {
+            this.price = price;
+            updateVersion();
+        }
+    }
+    public void changeItemStatus(ItemStatus itemStatus) {
+        if (!this.itemStatus.equals(itemStatus)) {
+            this.itemStatus = itemStatus;
+            updateVersion();
+        }
+    }
+
+    public void changeSequence(int sequence) {
+        this.sequence = sequence;
+    }
+
 }
