b02bb90e50d3ab9cd7b1559b952f2f4c852cee51
ramboramboram <boram_kim3@tmax.co.kr>
2023-08-24 16:05:01 +0900

    [feat] Option Repository 추가

---
+++ src/main/java/com/tmax/commerce/itemmodule/repository/option/OptionRepository.java
@@ -0,0 +1,7 @@
+package com.tmax.commerce.itemmodule.repository.option;
+
+import com.tmax.commerce.itemmodule.entity.option.Option;
+import com.tmax.commerce.itemmodule.repository.ExtendedRepository;
+
+public interface OptionRepository extends ExtendedRepository<Option, Long> {
+}
