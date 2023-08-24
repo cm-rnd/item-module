a321a2a836eecd1bd1f00a996881937bf4c109e1
hoyong1ee <hoyong_lee2@tmax.co.kr>
2023-08-24 19:46:20 +0900

    feat: Onsite, Pickup Item 생성자 추가

--- src/main/java/com/tmax/commerce/itemmodule/entity/item/subtyped/PickupItem.java
+++ src/main/java/com/tmax/commerce/itemmodule/entity/item/subtyped/PickupItem.java
@@ -1,5 +1,7 @@
 package com.tmax.commerce.itemmodule.entity.item.subtyped;
 
+import com.tmax.commerce.itemmodule.entity.file.File;
+import com.tmax.commerce.itemmodule.entity.file.FileDetail;
 import com.tmax.commerce.itemmodule.entity.item.Item;
 import com.tmax.commerce.itemmodule.entity.item.ItemStatus;
 import com.tmax.commerce.itemmodule.entity.item.OrderType;
@@ -9,8 +11,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 @Getter
 @Entity
@@ -20,5 +22,9 @@ public class PickupItem extends Item {
     @Builder
     public PickupItem(int price, boolean brandNew, boolean recommended, ItemStatus itemStatus, String name, String description, List<File> productImages, Item.SuperStoreStatus connectedSuperStore, int sequence) {
         super(price, brandNew, recommended, itemStatus, name, description, productImages, connectedSuperStore, sequence, OrderType.PICKUP);
+    }
+
+    public PickupItem(UUID uuid, int price, boolean brandNew, boolean recommended, ItemStatus itemStatus, String name, String description, List<FileDetail> itemImages, Boolean combinable) {
+        super(uuid, price, brandNew, recommended, itemStatus, name, description, itemImages, OrderType.PICKUP, combinable);
     }
 }
