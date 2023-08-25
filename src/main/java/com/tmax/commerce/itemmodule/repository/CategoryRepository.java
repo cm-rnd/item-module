81fdcafa34acd9626a368fcc12ad55df4853e59d
ramboramboram <boram_kim3@tmax.co.kr>
2023-08-26 02:18:01 +0900

    [feat] 필요 메서드 추가

--- src/main/java/com/tmax/commerce/itemmodule/repository/CategoryRepository.java
+++ src/main/java/com/tmax/commerce/itemmodule/repository/CategoryRepository.java
@@ -1,8 +1,14 @@
 package com.tmax.commerce.itemmodule.repository;
 
 import com.tmax.commerce.itemmodule.entity.category.Category;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface CategoryRepository extends ExtendedRepository<Category, Long>, CategoryRepositoryCustom {
 
@@ -11,5 +17,12 @@ public interface CategoryRepository extends ExtendedRepository<Category, Long>, 
 
     Boolean existsByName(String name);
 
+    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN true ELSE false END FROM Category c WHERE c.shopId = :shopId AND c.name = :name")
+    boolean existsByShopAndName(UUID shopId, String name);
+
+    Slice<Category> findByShopId(UUID shopId, Pageable pageable);
+
+    @Query("select max(c.sequence) from Category c WHERE c.shopId = :shopId")
+    Optional<Integer> findMaxSequence(@Param("shopId") UUID shopId);
 
 }
